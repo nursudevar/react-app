c3d1835694c33896e201ba3d23f64aa14e036130
Nur Sude Var <nursudevar@gmail.com>
2025-09-03 17:24:53 +0300

    create components, complete basics

---
+++ medium/src/components/MovieCard.jsx
@@ -0,0 +1,32 @@
+import { FaRegHeart } from "react-icons/fa";
+
+function MovieCard({movie}){
+
+
+    function onFavorite(){
+        alert("clicked");
+
+    }
+    return(
+
+        <div className="movie-card">
+            <div className="movie-poster">
+                <img src={movie.url} alt={movie.title}/>
+                <div className="movie-overlay">
+                    <button className="favorite-btn" onClick={onFavorite}>
+                       <FaRegHeart size={24} color="white" />
+                    </button>
+                </div>
+            </div>
+
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+                <p>{movie.release_date}</p>
+            </div>
+        </div>
+    )
+}
+
+
+
+export default MovieCard;
